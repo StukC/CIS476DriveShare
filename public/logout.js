b7cb42e3e4ba8c1d07c1a814ce8dbcdfa6dbf76d
StukC <85483301+StukC@users.noreply.github.com>
2024-04-06 23:55:10 -0400

    logout fix

--- public/logout.js
+++ public/logout.js
@@ -1,6 +1,6 @@
 function logout() {
     // Remove the token from localStorage or sessionStorage
-    localStorage.removeItem('token'); // Replace 'jwtToken' with the key you use to store the token
+    localStorage.removeItem('jwtToken'); // Replace 'jwtToken' with the key you use to store the token
 
     // Redirect to the login page
     window.location.href = '/login.html'; // Adjust if your login route is different
