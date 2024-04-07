2e2ec481d6e06cef6578c75193433006f8cc368e
StukC <85483301+StukC@users.noreply.github.com>
2024-04-07 01:35:37 -0400

    listcar

---
+++ models/carListing.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const carListingSchema = new mongoose.Schema({
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  model: String,
+  make: String,
+  year: Number,
+  mileage: Number,
+  features: [String],
+  location: {
+    type: { type: String, enum: ['Point'], default: 'Point' },
+    coordinates: { type: [Number], default: [0, 0] },
+  },
+  pricing: {
+    perDay: Number,
+  },
+  availability: [{ startDate: Date, endDate: Date }],
+  images: [String], // URLs to images of the car
+  createdAt: { type: Date, default: Date.now }
+});
+
+carListingSchema.index({ location: '2dsphere' });
+
+module.exports = mongoose.model('CarListing', carListingSchema);
